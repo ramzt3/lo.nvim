d26b3f483bdce0a5ea92519994460537f2e2b64d
tony <tonybanters@gmail.com>
2025-02-09 22:02:52 -0800

    Fixed other stuff and added binds

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,15 +1,24 @@
+function enable_transparency()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { bg= "none" })
+end
+
 return {
-  -- {
-  --   "Mofiqul/voscode.nvim",
-  --   name = 'vscode',
-  --   config = function()
-  --     vim.cmd.colorscheme "vscode"
-  --   end
-  -- }
-  {
-    "folke/tokyonight.nvim",
-    config = function()
-      vim.cmd.colorscheme "tokyonight"
-    end
-  }
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            vim.cmd.colorscheme "tokyonight" 
+            vim.cmd('hi Directory guibg=NONE')
+            vim.cmd('hi SignColumn guibg=NONE')
+            enable_transparency()
+        end
+    },
+    -- {
+    --   "Mofiqul/voscode.nvim",
+    --   name = 'vscode',
+    --   config = function()
+    --     vim.cmd.colorscheme "vscode"
+    --   end
+    -- }
 }
